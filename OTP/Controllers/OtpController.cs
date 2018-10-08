fbb24450d18a0a3f8483e0320a7b23fc4381c768
M.Hosseini <M.Hosseini@M-HOSSEINI>
2018-10-08 17:41:57 +0330

    Add project files.

---
+++ OTP/Controllers/OtpController.cs
@@ -0,0 +1,28 @@
+﻿using OtpNet;
+using System.Text;
+using System.Web.Http;
+
+namespace OTP.Controllers
+{
+    public class OtpController : ApiController
+    {
+        Totp _totp = new Totp(Encoding.ASCII.GetBytes("Pajoohesh"), 60, OtpHashMode.Sha512, 8);
+        [HttpGet]
+        public string Create()
+        {
+            return OtpGenerator.Create();
+        }
+
+        [HttpGet]
+        public int Remaining()
+        {
+            return OtpGenerator.Remaining();
+        }
+
+        [HttpGet]
+        public bool Verify(string otp)
+        {
+            return OtpGenerator.Verify(otp);
+        }
+    }
+}
---
+++ OTP/OTP/OtpGenerator.cs
@@ -0,0 +1,30 @@
+﻿using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace OTP
+{
+    public static class OtpGenerator
+    {
+        private static Totp _totp = new Totp(Encoding.ASCII.GetBytes("Pajoohesh"), 60, OtpHashMode.Sha512, 8);
+        public static string Create()
+        {
+            var result = _totp.ComputeTotp(DateTime.UtcNow);
+            return result;
+        }
+
+        public static int Remaining()
+        {
+            var result = _totp.RemainingSeconds(DateTime.UtcNow);
+            return result;
+        }
+        public static bool Verify(string otp)
+        {
+            var result = _totp.VerifyTotp(otp, out long timeWindowUsed, VerificationWindow.RfcSpecifiedNetworkDelay);
+            return result;
+        }
+    }
+}
